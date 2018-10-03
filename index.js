7e24397a4e679473e32a5c430bd7adedcd17b0a5
Phil Hartley <philh725@gmail.com>
2018-10-03 21:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === uppercase) {
-    
+  if (string === lowercase) {
+    return "I can't hear you!"
   }
+  
 }
